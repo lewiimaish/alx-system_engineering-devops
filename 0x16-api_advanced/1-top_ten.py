abb6516108a75f83d54a36caa9a95e563bb9dc0c
lewiimaish <mainalewis589@gmail.com>
2024-09-26 02:52:22 -0700

    Implement recursive function to count keywords in Reddit post titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 import requests
 
-
 def top_ten(subreddit):
+    """Function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
