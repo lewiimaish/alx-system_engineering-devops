40b8f6a67e4b36e3a6ca53bb4b14e3fd264c45a9
lewiimaish <mainalewis589@gmail.com>
2024-09-26 03:26:40 -0700

    Add script to retrieve number of subscribers for a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,21 @@
+
 #!/usr/bin/python3
-"""Function to query Reddit API and return no of subscribers"""
+'''function to querry subreddit'''
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Query reddit api and return no of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"}
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            subscribers = data['data']['subscribers']
+            return subscribers
         else:
-            return 0
-    except requests.RequestException:
-        return 0
+            return 0  # Invalid subreddit or other error
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0  # Return 0 if an exception occurs
